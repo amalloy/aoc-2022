d20687796c573af8cda08bb5a015e0c77de62afe
Alan Malloy <alan@malloys.org>
2023-03-20 19:32:21 -0700

    Day 3, part 1

---
+++ src/main/kotlin/org/malloys/akm/aoc2023/day3/Day3.kt
@@ -0,0 +1,42 @@
+package org.malloys.akm.aoc2023.day3
+
+import com.google.common.collect.ImmutableMultiset
+import com.google.common.collect.Multiset
+import com.google.common.collect.Multisets
+import org.malloys.akm.aoc2023.lib.readInput
+
+data class ItemType(val label: Char) {
+    val priority = when (label) {
+        in 'a'..'z' -> (label - 'a') + 1
+        in 'A'..'Z' -> (label - 'A') + 1 + 26
+        else -> throw RuntimeException("Bad label $label")
+    }
+}
+
+data class Rucksack(val left: ImmutableMultiset<ItemType>, val right: ImmutableMultiset<ItemType>) {
+    val overlap: Set<Multiset.Entry<ItemType>> = Multisets.intersection(left, right).entrySet()
+}
+
+fun String.toMultiSet(): ImmutableMultiset<ItemType> {
+    val builder = ImmutableMultiset.Builder<ItemType>()
+    for (x in chars()) {
+        builder.add(ItemType(x!!.toChar()))
+    }
+    return builder.build()
+}
+
+fun String.toRucksack(): Rucksack {
+    val size = length / 2
+    return Rucksack(substring(0, size).toMultiSet(), substring(size, length).toMultiSet())
+}
+
+fun main() {
+    val rucksacks = readInput(3).map { it.toRucksack() }
+    println("Part 1: ${part1(rucksacks)}")
+}
+
+fun part1(rucksacks: List<Rucksack>): Int {
+    return rucksacks.sumOf {
+        it.overlap.stream().findAny().orElseThrow { RuntimeException(it.toString()) }.element.priority
+    }
+}
