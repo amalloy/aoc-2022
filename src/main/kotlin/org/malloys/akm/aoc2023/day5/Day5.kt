efd2b3859d5a161793260fce16559011aa4c4e21
Alan Malloy <alan@malloys.org>
2023-03-21 21:28:43 -0700

    Day 5: Minor cleanups & style change

--- src/main/kotlin/org/malloys/akm/aoc2023/day5/Day5.kt
+++ src/main/kotlin/org/malloys/akm/aoc2023/day5/Day5.kt
@@ -1,12 +1,13 @@
 package org.malloys.akm.aoc2023.day5
 
 import org.malloys.akm.aoc2023.lib.readInput
-import java.util.ArrayDeque as ArrayDeque
+import java.util.ArrayDeque
+import java.util.stream.Collectors.joining
 
 typealias Stack = ArrayDeque<Char>
 
-data class Move(val quantity: Int, val source: Int, val destination: Int)
-data class Input(val stacks: List<Stack>, val moves: List<Move>)
+data class Move(val quantity : Int, val source : Int, val destination : Int)
+data class Input(val stacks : List<Stack>, val moves : List<Move>)
 
 fun main() {
     val input = parse(readInput(5))
@@ -14,52 +15,50 @@ fun main() {
     println("Part 2: ${part2(input)}")
 }
 
-fun runCrane(input: Input, applyMove: (move: Move, List<Stack>) -> Unit): String {
-    val stacks = input.stacks.map { it.clone() }
-    for (move in input.moves) {
-        applyMove(move, stacks)
-    }
-    return stacks.map { it.pollLast() }.joinToString(separator = "")
+fun runCrane(input : Input, applyMove : (Move, List<Stack>) -> Unit) : String {
+    val stacks = input.stacks.map {it.clone()}
+    input.moves.forEach {applyMove(it, stacks)}
+    return stacks.stream().map {it.pollLast().toString()}.collect(joining())
 }
 
-fun part1(input: Input): String {
-    return runCrane(input) { (quantity, source, destination), stacks ->
-        repeat(quantity) { stacks[source - 1].pollLast().let { stacks[destination - 1].addLast(it) } }
+fun part1(input : Input) : String {
+    return runCrane(input) {(quantity, source, destination), stacks ->
+        repeat(quantity) {stacks[source - 1].pollLast().let {stacks[destination - 1].addLast(it)}}
     }
 }
 
-fun part2(input: Input): String {
-    return runCrane(input) { (quantity, source, destination), stacks ->
+fun part2(input : Input) : String {
+    return runCrane(input) {(quantity, source, destination), stacks ->
         val buffer = Stack(ArrayDeque(quantity))
-        repeat(quantity) { stacks[source - 1].pollLast().let { buffer.addLast(it) } }
-        repeat(quantity) { buffer.pollLast().let { stacks[destination - 1].addLast(it) } }
+        repeat(quantity) {stacks[source - 1].pollLast().let {buffer.addLast(it)}}
+        repeat(quantity) {buffer.pollLast().let {stacks[destination - 1].addLast(it)}}
     }
 }
 
-fun parse(input: List<String>) = Input(parseStacks(input), parseMoves(input))
+fun parse(input : List<String>) = Input(parseStacks(input), parseMoves(input))
 
 private const val NUM_STACKS = 9
-fun parseStacks(input: List<String>): List<Stack> {
+fun parseStacks(input : List<String>) : List<Stack> {
     val crossSection = Regex("""(?:(?:\[[A-Z]]|\s{3})\s?){0,9}""")
-    val stacks = List(NUM_STACKS) { Stack(ArrayDeque()) }
+    val stacks = List(NUM_STACKS) {Stack(ArrayDeque())}
     input.filter {
         crossSection.matches(it)
-    }.flatMap { parseRow(it) }.forEach { (idx, label) ->
+    }.flatMap {parseRow(it)}.forEach {(idx, label) ->
         stacks[idx].addFirst(label)
     }
     return stacks
 }
 
 private val crate = Regex("""\[([A-Z])]\s?""")
-fun parseRow(row: String): List<Pair<Int, Char>> {
-    return row.chunked(4).flatMapIndexed { i, s ->
+fun parseRow(row : String) : List<Pair<Int, Char>> {
+    return row.chunked(4).flatMapIndexed {i, s ->
         val match = crate.matchEntire(s) ?: return@flatMapIndexed emptyList()
         val (label) = match.destructured
         listOf(Pair(i, label[0]))
     }
 }
 
-fun parseMoves(input: List<String>): List<Move> {
+fun parseMoves(input : List<String>) : List<Move> {
     val move = Regex("""move (\d+) from (\d) to (\d)""")
     return input.flatMap {
         val match = move.matchEntire(it) ?: return@flatMap emptyList()
