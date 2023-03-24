84241346b9b73433eb8e1a1c90ec05f9a023cf54
Alan Malloy <alan@malloys.org>
2023-03-24 05:53:23 -0700

    Day 10, part 2

--- src/main/kotlin/org/malloys/akm/aoc2022/day10/Day10.kt
+++ src/main/kotlin/org/malloys/akm/aoc2022/day10/Day10.kt
@@ -1,10 +1,13 @@
 package org.malloys.akm.aoc2022.day10
 
 import org.malloys.akm.aoc2022.lib.readInput
+import kotlin.math.abs
 
 sealed interface Instruction
 object Noop : Instruction
 data class Addx(val delta : Int) : Instruction
+
+data class RegisterInfo(val cycleNum : Int, val x : Int)
 
 val ADD_X = Regex("""addx (-?\d+)""")
 fun parse(line : String) =
@@ -18,18 +21,35 @@ fun parse(line : String) =
 fun main() {
     val program = readInput(10).map {parse(it)}
     println("Part 1: ${part1(program)}")
+    println("Part 2: \n${part2(program)}")
 }
 
-fun part1(program : List<Instruction>) : Int {
+private fun registerInfoByCycle(program : List<Instruction>) : Sequence<RegisterInfo> {
     val tickChanges = program.asSequence().flatMap {instruction ->
         when (instruction) {
             is Addx -> sequenceOf(0, instruction.delta)
             Noop -> sequenceOf(0)
         }
     }
-    val xs = sequence {
-        yield(0)
-        yieldAll(tickChanges.scan(1, Int::plus))
-    }
-    return xs.mapIndexed {cycleNum, x -> x * cycleNum}.filterIndexed {cycleNum, x -> (cycleNum % 40) == 20}.sum()
+    return tickChanges
+        .scan(1, Int::plus)
+        .mapIndexed {cycleNum, x -> RegisterInfo(cycleNum = cycleNum + 1, x = x)}
 }
+
+fun part1(program : List<Instruction>) : Int =
+    registerInfoByCycle(program)
+        .filter {reg -> with(reg) {(cycleNum % 40) == 20}}
+        .map {reg -> with(reg) {cycleNum * x}}.sum()
+
+fun part2(program : List<Instruction>) =
+    registerInfoByCycle(program).map {reg ->
+        with(reg) {
+            val columnBeingDrawn = (cycleNum - 1) % 40
+            abs(x - columnBeingDrawn) <= 1
+        }
+    }.map {
+        when (it) {
+            true -> "#"
+            false -> "."
+        }
+    }.chunked(40).map {it.joinToString(separator = "")}.joinToString(separator = "\n")
