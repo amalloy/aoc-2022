f3c05937f950e67260cea1be69d73a5e73c77f76
Alan Malloy <alan@malloys.org>
2023-03-24 01:54:58 -0700

    Allow adding and subtracting Coordinates

--- src/main/kotlin/org/malloys/akm/aoc2022/lib/Coordinates.kt
+++ src/main/kotlin/org/malloys/akm/aoc2022/lib/Coordinates.kt
@@ -2,6 +2,8 @@ package org.malloys.akm.aoc2022.lib
 
 data class Coordinate(val x : Int, val y : Int) {
     operator fun plus(dir : Direction) : Coordinate = with(dir) {Coordinate(x = x + dx, y = y + dy)}
+    operator fun plus(other : Coordinate) : Coordinate = Coordinate(x = x + other.x, y = y + other.y)
+    operator fun minus(other : Coordinate) : Coordinate = Coordinate(x = x - other.x, y = y - other.y)
 }
 
 enum class Direction(val dx : Int, val dy : Int) {
