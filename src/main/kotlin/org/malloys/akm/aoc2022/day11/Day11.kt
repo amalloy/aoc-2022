3e638d7969b7e781bc1f4345cf557b3be844e7f2
Alan Malloy <alan@malloys.org>
2023-03-25 03:06:49 -0700

    Day 11, part 1

--- src/main/kotlin/org/malloys/akm/aoc2022/day11/Day11.kt
+++ src/main/kotlin/org/malloys/akm/aoc2022/day11/Day11.kt
@@ -1,19 +1,36 @@
 package org.malloys.akm.aoc2022.day11
 
+import com.google.common.collect.Comparators
+import com.google.common.collect.ImmutableList
 import org.malloys.akm.aoc2022.lib.readInput
 import java.util.ArrayDeque
-import java.util.Queue
 
 typealias Item = Int
 typealias MonkeyIndex = Int
 
 data class Monkey(
-    val items : Queue<Item>,
+    val items : ArrayDeque<Item>,
     val operation : (Item) -> Item,
     val factor : Int,
     val ifTrue : MonkeyIndex,
-    val ifFalse : MonkeyIndex
-)
+    val ifFalse : MonkeyIndex,
+) {
+    var itemsHandled : Int = 0
+    fun doTurn() : List<Pair<MonkeyIndex, Item>> {
+        val ret = ImmutableList.builder<Pair<MonkeyIndex, Item>>()
+        while (items.isNotEmpty()) {
+            val new = operation(items.removeFirst()) / 3
+            val recipient = if (new % factor == 0) {
+                ifTrue
+            } else {
+                ifFalse
+            }
+            ret.add(Pair(recipient, new))
+            itemsHandled++
+        }
+        return ret.build()
+    }
+}
 
 fun parseMonkey(lines : List<String>) : Monkey {
     return Monkey(
@@ -26,7 +43,7 @@ fun parseMonkey(lines : List<String>) : Monkey {
 }
 
 val ITEMS = Regex("""Starting items: ((?:\d+(?:, )?)+)""")
-fun parseItems(s : String) : Queue<Item> =
+fun parseItems(s : String) : ArrayDeque<Item> =
     ITEMS.matchEntire(s)?.destructured?.let {(items) ->
         items.splitToSequence(", ").map {it.toInt()}.toCollection(ArrayDeque())
     } ?: throw RuntimeException("No parse: $s")
@@ -66,5 +83,19 @@ fun parseIfFalse(s : String) = parseCond("false", s)
 
 fun main() {
     val monkeys = readInput(11).chunked(7).map {parseMonkey(it)}
-    println(monkeys)
+    println("Part 1: ${simulateRounds(20, monkeys)}")
+}
+
+fun simulateRounds(numRounds : Int, initMonkeys : List<Monkey>) : Int {
+    val lcm = initMonkeys.stream().map {it.factor}.reduce(Int::times).orElseThrow()
+    val monkeys = initMonkeys.map {it.copy(items = it.items.clone())}
+    repeat(numRounds) {
+        for (monkey in monkeys) {
+            for ((recipient, item) in monkey.doTurn()) {
+                monkeys[recipient].items.addFirst(item % lcm)
+            }
+        }
+    }
+    return monkeys.stream().map {it.itemsHandled}.collect(Comparators.greatest(2, naturalOrder<Item>()))
+        .reduce(Int::times)
 }
